f039197b477b683683022618804cad20d9584277
vinayaksapple <vinayakkr9304@gmail.com>
2025-12-30 18:42:11 +0530

    mid question

--- src/Collection/List/CollectionPracticeMid.kt
+++ src/Collection/List/CollectionPracticeMid.kt
@@ -13,6 +13,62 @@ package Collection.List
 //Convert a list of pairs into a map.
 //Check if two lists contain the same elements ignoring order.
 
+fun getGRouped(){
+    val words = listOf("apple", "banana", "avocado", "cat")
+
+    val grouped = words.groupBy { it.first() }
+    println(grouped)
+
+}
+
+
+fun getMax(){
+    val numbers = listOf(3, 7, 2, 9, 4)
+    var max = numbers[0]
+    for(i in numbers){
+        if(i > max) max = i
+    }
+    println(max)
+}
+
+
+fun mergeList(){
+    val list1 = listOf(1, 2, 3)
+    val list2 = listOf(3, 4, 5)
+    val newList = list1+list2
+    println(newList.distinct())
+}
+
+fun freqWord(){
+    val sentence = "kotlin is fun and kotlin is powerful"
+    val newMap = mutableMapOf<String, Int>()
+    for(i in sentence.split(" " )){
+        newMap[i] = newMap.getOrDefault(i,0)+1
+    }
+    println(newMap)
+}
+
+
+fun getAppearOnce(){
+    val numbers = listOf(1, 2, 2, 3, 4, 4)
+    val newMap = mutableMapOf<Int, Int>()
+    for(i in numbers){
+        newMap[i] = newMap.getOrDefault(i,0)+1
+
+    }
+    println(newMap.filter { it.value == 1 })
+}
+
+fun listToMap(){
+    val numbers = listOf(2,3,4,5,6,7,8,96,5,4,3,2,2,1)
+    val newMap = mutableMapOf<Int, Int>()
+    for (i in numbers){
+        newMap[i] = newMap.getOrDefault(i,i*i)
+    }
+    val filter = newMap.filter { it.value == 1 }
+    println(filter.keys)
+}
+
 
 fun firstInlist(){
     val numbers = listOf(2,3,4,5,6,7,8,96,5,4,3,2,2,1)
@@ -36,4 +92,10 @@ fun countFreq(){
 fun main(){
     countFreq()
     firstInlist()
+    listToMap()
+    getAppearOnce()
+    freqWord()
+    mergeList()
+    getMax()
+    getGRouped()
 }
