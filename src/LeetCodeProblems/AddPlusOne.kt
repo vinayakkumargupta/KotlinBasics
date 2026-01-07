a68a61362c56e28dd8f322fd8166d8f941e38ce3
vinayaksapple <vinayakkr9304@gmail.com>
2026-01-07 23:44:09 +0530

    leetcode question 1

--- src/LeetCodeProblems/AddPlusOne.kt
+++ src/LeetCodeProblems/AddPlusOne.kt
@@ -1,17 +1,8 @@
 package LeetCodeProblems
 
 
-fun addPlus1( ar : IntArray) : IntArray{
-    for(i in ar.size-1 downTo 0){
-        if(ar[i]<9){
-            ar[i]++
-            return ar
-        }
-        ar[i]= 0
-    }
-    val result = IntArray(ar.size + 1)
-    result[0] = 1
-    return result
+fun addPlus1(ar : IntArray) : IntArray{
+
 }
 
 
