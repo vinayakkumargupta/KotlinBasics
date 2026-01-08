e937778ef9b192fe6e8b6a3d22386344dd7a5291
vinayaksapple <vinayakkr9304@gmail.com>
2026-01-08 23:06:33 +0530

    leetcode question 1

--- src/LeetCodeProblems/AddPlusOne.kt
+++ src/LeetCodeProblems/AddPlusOne.kt
@@ -1,12 +1,28 @@
 package LeetCodeProblems
 
 
-fun addPlus1(ar : IntArray) : IntArray{
+//fun addPlus1(ar : IntArray) : IntArray{
+//
+//}
+fun countPrimes(n: Int): Int {
+    if (n <= 2) return 0
 
+    val isPrime = BooleanArray(n) { true }
+    isPrime[0] = false
+    isPrime[1] = false
+
+    for (i in 2 until Math.sqrt(n.toDouble()).toInt() + 1) {
+        if (isPrime[i]) {
+            for (j in i * i until n step i) {
+                isPrime[j] = false
+            }
+        }
+    }
+
+    return isPrime.count { it }
 }
 
-
 fun main(){
-    println(addPlus1(intArrayOf(2,3,9)).joinToString())
+    println(countPrimes(12))
 
 }
